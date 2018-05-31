5c0e86036b1135f3cbea441374c4e7a7e9428960
Samir-krivalic <samirkrivalic@gmail.com>
2018-05-31 22:07:58 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  // secret menu toggle
+  $('.nav-toggle').click(function() {
+    $('.main-nav').toggleClass('is-open');
+    $('.sec').toggleClass('is-open');
+  })
+
+})
